90f4d961f06ec6acd13a25811e688686852f8da2
MariiaS <47188479+MariiaShpuntova@users.noreply.github.com>
2019-01-30 12:53:34 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+let balance = 0;
+let name = `John Doe`;
+
+
+// Greet the user by name
+      // Search the document for the 'username' element
+      let _username = document.getElementById('username');
+      // Change the content of the element
+      _username.innerHTML = name;
+
+      // The above can be simplified:
+      //    document.getElementById('username').innerHTML = name;
+
+
+
+// 1. Let them know their starting balance
+      // Search the document for the 'balance' element
+      // Change the content of the element
+      //I was thinking to use this web-site in oder to fix this problem, but it did not work for me()//https://ethereum.stackexchange.com/questions/8900/how-to-get-transactions-by-account-using-web3-js
+
+
+var completeTransaction = function() {
+
+  // Store the value from the 'amount' input in a variable (let)
+        // Make sure it's a number, not a string!!!  parseInt()
+  let _amount = parseInt(document.getElementById('amount').value);
+
+  // 1. Update the balance using the transaction value
+  balance += 50;
+
+  // 2. Add a record to the history showing the amount deposited
+  document.getElementById('history').innerHTML += `<li>You deposited $50.</li>`;
+  //not sure if that is right, cause they all 3 showing up
+  //document.getElementById('history').innerHTML += `<li>You deposited $100.</li>`;
+  //document.getElementById('history').innerHTML += `<li>You deposited $150.</li>`;
+
+  // Update the balance in the document
+  document.getElementById('balance').innerHTML = balance;
+
+  // 3. IF the balance is less than 0, make the balance red, else make it black
+        // A) change the style.color of the balance
+        // B) create a css class and .add() a class to the balance
+        //if (udata< 0) {
+
+
+
+       //A)//Also I used this web-site//(https://www.w3schools.com/js/js_if_else.asp)
+       //I did use this web-site but it did not work out, probably I made a mistake somewhere)//(https://stackoverflow.com/questions/25713190/change-div-colour-with-js-if-else-conditiond)
+        //document.getElementById("balance").style.backgroundColor = 'red';
+        //}
+        //else(udata >0 udata.value <70)
+        //document.getElementById("balance").style.backgroundColor = 'black';
+        //{
+
+      //B)//
+
+};
+
+document.getElementById('process').addEventListener('click', completeTransaction)
+
+// EVENT:
+//   1) Which object should it be listening to?     document.getElementById('process')
+//   2) What type of event are we waiting for?      'click'
+//   3) What do we do when it happens? (callback)   completeTransaction
+
+
+
+// Note: Both records must be visible
